b1462104145619bceca29650f7a33eecc3e9d9af
eyatsenko <eduardyacenko@gmail.com>
2024-02-05 13:35:39 +0200

    Test

--- src/ExampleTest.java
+++ src/ExampleTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class ExampleTest {
     @Test
     public void Tests() {
+
         assertEquals("makeReadable(0)", "00:00:00", HumanReadableTime.makeReadable(0));
         assertEquals("makeReadable(5)", "00:00:05", HumanReadableTime.makeReadable(5));
         assertEquals("makeReadable(60)", "00:01:00", HumanReadableTime.makeReadable(60));
